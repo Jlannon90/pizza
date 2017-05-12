931f2fea47886aa0f5904d73a9897ea2f380e224
James Lannon <jamesmlannon1990@live.com>
2017-05-12 09:11:03 -0700

    add user interface logic to gather size and topping selections once the form is submitted, add variable newPizza to create a new Pizza object when the form is submitted, edit the name of the radio boxes on the html

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,7 @@ Pizza.prototype.typeOfPizza = function() {
   return "1x " + this.pizzaSize + " with " + this.pizzaToppings;
 }
 
+// pizza prototype that calculates cost
 Pizza.prototype.cost = function() {
   var pizzaCost = 10;
 
@@ -28,6 +29,15 @@ Pizza.prototype.cost = function() {
   return PizzaCost;
 }
 
-
-
 // user interface logic
+
+$(function() {
+  $("form#make-pizza").submit(function(event) {
+    event.preventDefault();
+      var sizeSelection = $("input:radio[name=size]:checked").val();
+      var toppingSelection = $("input:radio[name=topping]:checked").val();
+
+      var newPizza = new Pizza(sizeSelection, toppingSelection);
+
+  });
+});
